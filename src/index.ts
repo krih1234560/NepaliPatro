a757787548da433db926aed3663058267e629c7b
Krishna Majhi <96642265+krih1234560@users.noreply.github.com>
2026-08-22 14:30:43 +0545

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,29 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    try {
+    try {export default {
+  async fetch(request, env, ctx) {
+    // write a key-value pair
+    await env.KV.put('KEY', 'VALUE');
+
+    // read a key-value pair
+    const value = await env.KV.get('KEY');
+
+    // list all key-value pairs
+    const allKeys = await env.KV.list();
+
+    // delete a key-value pair
+    await env.KV.delete('KEY');
+
+    // return a Workers response
+    return new Response(
+      JSON.stringify({
+        value: value,
+        allKeys: allKeys,
+      }),
+    );
+  } 
+}
       const url = new URL(request.url);
       const path = url.pathname;
 
